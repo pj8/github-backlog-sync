69ef8162d8abe94953b5f89e1d42a7ade6ded885
Yuki Adachi <yuki777@gmail.com>
2023-05-19 17:49:58 +0900

    Support for null values in custom fields

--- sync.js
+++ sync.js
@@ -14,20 +14,24 @@ const github = require('@actions/github');
 
   for (const backlogIssueMatch of backlogIssueMatches) {
     const backlogIssueKey = backlogIssueMatch[1];
-    console.log(`backlogIssueKey: ${backlogIssueKey}`);
     const backlogIssueUrl = `https://${process.env.BACKLOG_SPACE_NAME}.backlog.jp/api/v2/issues/${backlogIssueKey}?apiKey=${process.env.BACKLOG_API_KEY}`;
     try {
       const currentIssueDetails = await axios.get(backlogIssueUrl);
       let currentCustomFieldValue = currentIssueDetails.data.customFields.find(field => field.id === Number(process.env.BACKLOG_CUSTOM_FIELD_ID)).value;
-      const lines = currentCustomFieldValue.split('\n');
-
-      if (lines.some(line => line === pr.html_url)) {
-        console.log(`The URL is already in the custom field: ${pr.html_url}`);
-        continue; // Skip to next iteration if the URL is already in the custom field.
+      if(currentCustomFieldValue !== null) {
+        const lines = currentCustomFieldValue.split('\n');
+        if (lines.some(line => line === pr.html_url)) {
+          continue; // Skip to next iteration if the URL is already in the custom field.
+        }
       }
 
       // Add new URL and update the issue if the URL is not in the custom field.
-      currentCustomFieldValue += `\n${pr.html_url}`;
+      console.log(`Update backlogIssueKey: ${backlogIssueKey}`);
+      if(currentCustomFieldValue === null) {
+        currentCustomFieldValue = `${pr.html_url}`;
+      }else{
+        currentCustomFieldValue += `\n${pr.html_url}`;
+      }
       const data = querystring.stringify({ [`customField_${process.env.BACKLOG_CUSTOM_FIELD_ID}`]: currentCustomFieldValue });
       await axios.patch(backlogIssueUrl, data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
     } catch (error) {
