c468c976e5d6dd841827d30234384723f2aaf7f7
Yuki Adachi <yuki777@gmail.com>
2023-05-19 10:20:38 +0900

    Add new URL and update the issue if the URL is not in the custom field.

--- sync.js
+++ sync.js
@@ -7,12 +7,15 @@ const github = require('@actions/github');
   // const context = {
   //   payload: {
   //     pull_request: {
-  //       body: 'Here is the body of the PR. https://spaceName.backlog.jp/view/projectKey-4',
-  //       html_url: 'https://github.com/org/repo/pull/1',
+  //       body: '',
+  //       html_url: '',
   //     },
   //   },
   // };
+
+  // production
   const { context } = github;
+
   const pr = context.payload.pull_request;
   const prBody = pr.body;
   const backlogIssueMatch = prBody.match(new RegExp(`https?:\/\/[^\s]*${process.env.BACKLOG_SPACE_NAME}\.backlog\.jp\/view\/(${process.env.BACKLOG_PROJECT_KEY}-\\d+)`));
@@ -22,11 +25,17 @@ const github = require('@actions/github');
     const backlogIssueUrl = `https://${process.env.BACKLOG_SPACE_NAME}.backlog.jp/api/v2/issues/${backlogIssueKey}?apiKey=${process.env.BACKLOG_API_KEY}`;
     try {
       const currentIssueDetails = await axios.get(backlogIssueUrl);
-      const currentCustomFieldValue = currentIssueDetails.data.customFields.find(field => field.id === Number(process.env.BACKLOG_CUSTOM_FIELD_ID)).value;
-      if (currentCustomFieldValue !== pr.html_url) {
-        const data = querystring.stringify({ [`customField_${process.env.BACKLOG_CUSTOM_FIELD_ID}`]: pr.html_url });
-        await axios.patch(backlogIssueUrl, data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+      let currentCustomFieldValue = currentIssueDetails.data.customFields.find(field => field.id === Number(process.env.BACKLOG_CUSTOM_FIELD_ID)).value;
+      const lines = currentCustomFieldValue.split('\n');
+
+      if (lines.some(line => line === pr.html_url)) {
+        return; // Early return if the URL is already in the custom field.
       }
+
+      // Add new URL and update the issue if the URL is not in the custom field.
+      currentCustomFieldValue += `\n${pr.html_url}`;
+      const data = querystring.stringify({ [`customField_${process.env.BACKLOG_CUSTOM_FIELD_ID}`]: currentCustomFieldValue });
+      await axios.patch(backlogIssueUrl, data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
     } catch (error) {
       console.error(`Failed to update Backlog issue: ${error}`);
     }
